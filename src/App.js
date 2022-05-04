45843f9af613b06bea1e0af90716d11c89b455f9
Atiqur Rahman <atiqit52@gmail.com>
2022-05-04 22:45:39 +0600

    header set

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 
+import { Routes } from 'react-router-dom';
 import './App.css';
+import Header from './Components/Shared/Header/Header';
 
 function App() {
   return (
-    <h1 className="text-6xl font-bold underline bg-yellow-400">
-    Hello world!
-  </h1>
+   <>
+   <Header> </Header>
+   <Routes>
+
+   </Routes>
+   </>
 
   );
 }
