fff0b18cb97e517c538fde066c441659812591d7
Atiqur Rahman <atiqit52@gmail.com>
2022-05-08 21:48:06 +0600

    readme file set

--- src/Components/Home/Banner/Banner.js
+++ src/Components/Home/Banner/Banner.js
@@ -4,7 +4,7 @@ import banner1 from '../../../assets/banner/banner.jpg'
 const Banner = () => {
     return (
         <div>
-            <img className='h-80 w-[1300px]' src={banner1} alt="" />
+            <img className='h-120 w-[1300px]' src={banner1} alt="" />
            
         </div>
     );
